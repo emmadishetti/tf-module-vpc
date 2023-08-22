c72057451f80ef42df38f532c7257b66d75a1972
suresh <surya2fun@gmail.com>
2023-08-22 21:17:20 +0530

    terraform module roboshop

--- subnets/main.tf
+++ subnets/main.tf
@@ -8,3 +8,18 @@ resource "aws_subnet" "main" {
     name = each.key
   }
 }
+
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_route_table_association" "route" {
+  for_each = var.subnets
+  subnet_id      = lookup(lookup(aws_subnet.main, each.key, null), "id", null)
+  route_table_id = lookup(lookup(aws_route_table.main, each.key, null), "id", null)
+}
